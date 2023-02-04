4ac797ccfddd15dc8af8fcdc39cea5a0847f8ab9
gutto-moura <gmourah1792@gmail.com>
2023-02-04 10:06:21 -0300

    Arquivos iniciais e conexão com o banco

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const connection = require("./database/connection");
+const categoriesController = require("./categories/categoriesController");
+const articlesController = require("./articles/articlesController");
+
+connection.authenticate().then(() => {
+  console.log("CONECTADO AO BANCO blogpress")
+}).catch((err) => {
+  console.log(err)
+});
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use("/", categoriesController);
+app.use("/", articlesController);
+
+app.get("/", (req, res) => {
+  res.render('index');
+})
+
+
+app.listen(8800, () => {
+  console.log("RODANDO NA PORTA 8800")
+});
