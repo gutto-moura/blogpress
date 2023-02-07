aa5421d75e390282402dc3247871a25e6f959d07
gutto-moura <gmourah1792@gmail.com>
2023-02-07 17:22:46 -0300

    finalizando a pagina de artigos

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -1,9 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../categories/Category");
+const Article = require("./Article");
+const slugify = require("slugify");
 
-router.get("/admin/articles/new", (req, res) => {
-  res.render("admin/articles/new");
+router.get("/admin/articles", (req, res) => {
+  res.render("admin/articles/indexSS")
 })
 
+router.get("/admin/articles/new", (req, res) => {
+  Category.findAll().then((categories) => {
+    res.render("admin/articles/new", { categories: categories });
+  });
+})
+router.post("/admin/save", (req, res) => {
+  var title = req.body.title;
+  var body = req.body.body;
+  var category = req.body.category;
+
+  Article.create({
+    title: title,
+    slug: slugify(title),
+    body: body,
+    categoryId: category
+  }).then(() => {
+    res.redirect("/admin/articles")
+  })
+})
 
 module.exports = router;
