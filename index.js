e12dae84c2a9ba840319e2be2cabc05228d25266
gutto-moura <gmourah1792@gmail.com>
2023-02-13 18:25:10 -0300

    finalizando a página home

--- index.js
+++ index.js
@@ -25,11 +25,35 @@ app.use("/", categoriesController);
 app.use("/", articlesController);
 
 app.get("/", (req, res) => {
-  Article.findAll().then((articles) => {
-    res.render('index', {articles: articles});
+  Article.findAll({
+    order:[ 
+      ['id', 'DESC']
+    ]
+  }).then((articles) => {
+    Category.findAll().then((categories) => {
+      res.render('index', {articles: articles, categories:categories});
+    })
   })
+});
 
-})
+app.get("/:slug", (req, res) => {
+  var slug = req.params.slug;
+  Article.findOne({
+    where:{
+      slug: slug
+    }
+  }).then((article) => {
+    if(article != undefined){
+      Category.findAll().then((categories) => {
+        res.render('articles', {article: article, categories:categories});
+      })
+    }else{
+      res.redirect("/");
+    }
+  }).catch(err => {
+    res.redirect("/");
+  })
+});
 
 app.listen(8800, () => {
   console.log("RODANDO NA PORTA 8800")
