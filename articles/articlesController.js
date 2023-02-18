59bb625a00599dcfbeea98b0bd38af1fd0c7f0c7
gutto-moura <gmourah1792@gmail.com>
2023-02-18 18:52:45 -0300

    criando paginação

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -84,4 +84,40 @@ router.post("/articles/update", (req, res) => {
     res.redirect("/admin/articles");
   });
 })
+
+router.get("/articles/page/:num", (req,res) => {
+  var page = req.params.num;
+  var offset = 0;
+
+  if(isNaN(page) || page == 1){
+    offset = 0;
+  }else{
+    offset = parseInt(page);
+  }
+
+  Article.findAndCountAll({
+    limit: 4,
+    offset: offset,
+    order:[ 
+      ['id', 'DESC']
+    ],
+  }).then(articles => {
+    var next; 
+    if(offset + 4 >= articles.count){
+      next = false;
+    }else{
+      next = true;
+    }
+
+    var result = {
+      next: next,
+      articles: articles
+    }
+    Category.findAll().then(categories => {
+        res.render("admin/articles/page", {result: result, categories:categories})
+    })
+  })
+})
+
+
 module.exports = router;
