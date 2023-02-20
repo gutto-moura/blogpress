cb1e1e734be0e9e6fbb2c000073f4ae839a388db
gutto-moura <gmourah1792@gmail.com>
2023-02-20 09:30:31 -0300

    Finalizando paginação

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -92,7 +92,7 @@ router.get("/articles/page/:num", (req,res) => {
   if(isNaN(page) || page == 1){
     offset = 0;
   }else{
-    offset = parseInt(page);
+    offset = (parseInt(page)-1) * 4;
   }
 
   Article.findAndCountAll({
@@ -110,6 +110,7 @@ router.get("/articles/page/:num", (req,res) => {
     }
 
     var result = {
+      page: parseInt(page),
       next: next,
       articles: articles
     }
